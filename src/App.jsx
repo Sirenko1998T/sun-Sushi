99158071f78125548bc2edcebd3f6755cfef76a3
Sirenko1998T <sirenko23t@gmail.com>
2025-10-06 17:28:23 +0300

    add basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Main from './pages/main'
+
+let root = document.getElementById('root')
+ReactDOM.createRoot(root).render(
+	<React.StrictMode>
+		<Main />
+	</React.StrictMode>
+)
+
